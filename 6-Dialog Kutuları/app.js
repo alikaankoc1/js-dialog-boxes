fb3412320294986d2acf875b5a9cacafdfc8901e
alikaankoc1 <kocalikaan564@gmail.com>
2025-03-04 01:54:43 +0300

    js-dialog-boxes
    js-dialog-boxes-example

---
+++ 6-Dialog Kutuları/app.js
@@ -0,0 +1,22 @@
+//! Alert: In JavaScript, alert() displays a popup window with a warning message.
+alert("Refresh the page with the F5 key.");
+
+//! Prompt: In JavaScript, prompt() displays a popup window to get input from the user.
+
+let age = prompt("Enter your age:");
+console.log(age);
+console.log(typeof age);
+//? Values ​​received from the user are always returned as strings.
+
+//! Confirm: In JavaScript, confirm() displays a confirmation box where the user can respond with "OK" or "Cancel."
+
+let approval = confirm("Do you confirm the transaction?");
+console.log(approval);
+console.log(typeof approval);
+
+
+
+
+
+
+
